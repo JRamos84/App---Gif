4171592b9ddfdf0911314b31ae8a14947b286b79
JRamos84 <joseph0001@gmail.com>
2023-03-07 08:32:36 -0300

    uso de useRef, hook lazyTrems 54:10

--- src/componentes/TrendingSearches/index.js
+++ src/componentes/TrendingSearches/index.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useState, useRef } from 'react'
-import getTrendingTerms from '../../services/getTrendingTermsService'
+import React, { useEffect, useState } from 'react'
+import getTrendingTerms from 'services/getTrendingTermsService'
 import Category from '../Category'
+import useNearScrenn from 'hooks/useNearScreen'
 
 function TrendingSearches() {
     const [trends, setTrends] = useState([])
@@ -13,28 +14,16 @@ function TrendingSearches() {
     return <Category name='Tendencias' options={trends} />
 }
 
+
+
+
 export default function LazyTrending() {
-    const [show, setShow] = useState(false)
-    const elementRef = useRef()
+    const {isNearScreen, fromRef}= useNearScrenn({distance:'150px'})
 
-    useEffect(function () {
-        const onChange = (entries, observer) => {
 
-            const el = entries[0]
-            if (el.isIntersecting) {
-                setShow(true)
-                observer.disconnect()
-            }
+    
 
-        }
-        const observer = new IntersectionObserver(onChange, {
-            rootMargin: '100px'
-        })
-        observer.observe(elementRef.current)
-        return ()=> observer.disconnect()
-    }, [])
-
-    return <div ref={elementRef}>
-        {show ? <TrendingSearches /> : null}
+    return <div ref={fromRef}>
+        {isNearScreen ? <TrendingSearches /> : null}
     </div>
 }
